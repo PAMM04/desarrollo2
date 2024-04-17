dc43873a2dd443fe7cf016aeb95a1707c6b57cec
PAMM04 <166728954+PAMM04@users.noreply.github.com>
2024-04-17 08:14:31 -0400

    Delete javascript00.js

--- javascript00.js
+++
@@ -1,9 +0,0 @@
-//comentario de una linea
-/*comentario multiples*/
-console.log("Hola mundo");
-/*
-Descripcion: Ejemplo de Hola Mundo por consola.;log
-Desarrolloador; Nombre y apellido
-Fecha; Formato gringo 2024.04.15
-Cambios; Ninguno
-*/
